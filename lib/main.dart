9e34bdc82254407315c61c1c28a812d9493db496
Kholil Mustofa <kholilmoestofa954@gmail.com>
2025-12-26 07:17:13 +0700

    Complete Firebase setup: add Android config, permissions, and activate Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:thriftly_app/config/app_theme.dart';
 import 'package:thriftly_app/screens/splash_screen.dart';
 import 'package:thriftly_app/services/auth_service.dart';
 import 'package:thriftly_app/services/firestore_service.dart';
 import 'package:thriftly_app/services/storage_service.dart';
-// Import ini akan dibuat setelah run flutterfire configure
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 
 void main() async {
   // Ensure Flutter binding is initialized
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Firebase
-  // IMPORTANT: Uncomment this after running `flutterfire configure`
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const ThriftlyApp());
 }
